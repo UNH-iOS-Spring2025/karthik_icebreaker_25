c20e23301300d963624d01179bd8279275d041dc
Saikarthikreddy05 <sodur1@unh.newhaven.edu>
2025-01-30 20:04:09 -0500

    Added Button
    i have added the Login button

--- karthik_icebreaker_25/ContentView.swift
+++ karthik_icebreaker_25/ContentView.swift
@@ -13,12 +13,31 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Text("Hello, class!")
+            Text("This is sai's iphone")
+            Image(systemName: "heart.fill")
+                .foregroundColor(.red)
+                .padding()
+            
+            Button(action: {
+                            print("Login button tapped!")
+                        }) {
+                            Text("Login")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(width: 150, height: 50)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                    }
+                    .padding()
+                }
+            }
 
-#Preview {
-    ContentView()
-}
+            struct ContentView_Previews: PreviewProvider {
+                static var previews: some View {
+                    ContentView()
+                }
+            }
+
